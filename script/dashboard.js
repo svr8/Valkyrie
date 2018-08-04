d7ef6927a1e9369a38c099637ac8ecdb234a2294
Shikhar <shikhar.vaish90@gmail.com>
2018-08-04 20:07:15 +0530

    Updated quiz page

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    //Sidebar links
+    //Dashboard links
     $("#sidebar-student-details").on('click', function() { console.log('Redirect to student-details'); });
     $("#sidebar-home").on('click', function() { console.log('Redirect to home'); });
     $("#sidebar-settings").on('click', function() { console.log('Redirect to settings'); });
