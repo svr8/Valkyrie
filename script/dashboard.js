13e39efaf3274ef4b98106811ed695db530abcb2
Shikhar <shikhar.vaish90@gmail.com>
2018-08-04 20:02:10 +0530

    Updated quiz page

---
+++ script/dashboard.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    //Sidebar links
+    $("#sidebar-student-details").on('click', function() { console.log('Redirect to student-details'); });
+    $("#sidebar-home").on('click', function() { console.log('Redirect to home'); });
+    $("#sidebar-settings").on('click', function() { console.log('Redirect to settings'); });
+});
