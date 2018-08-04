348c3587f25fcfcf5facec3e9e4e3286e71c63f8
Shikhar <shikhar.vaish90@gmail.com>
2018-08-04 23:06:08 +0530

    Updated quiz retrieval

--- script/quiz.js
+++ script/quiz.js
@@ -1,11 +1,47 @@
+var questions = {
+    "A": {    
+        "id": "0",
+        "title": "A",
+        "text": "Question0",
+        "code_template": "Template",
+        "time_limit": "1s"
+    },
+    "B": {
+        "id": "1",
+        "title": "B",
+        "text": "Question1",
+        "code_template": "Template"
+    },
+    "C": {
+        "id": "1",
+        "title": "C",
+        "text": "Question1",
+        "code_template": "Template"
+    },
+
+};
+
+
 $(document).ready(function(){
     //Question Links
-    // for(var i=0;i<n;i++) {
-    //     $("sidebar-questionIndex").on('click', function(){ console.log('Question Index Pressed'); });
-    // }
+    for( var i in questions) loadQuestion(questions[i]);
     
     // //Solution Links
     // for(var i=0;i<n;i++) {
     //     $("sidebar-solutionIndex").on('click', function(){ console.log('Solution Index Pressed'); });
     // }
 });
+
+// question is a JSON Object with following structure:
+// {
+//      "id":   "XYZ",
+//      "title": "XYZ",
+//      "text": "XYZ",
+//      "code_template": "XYZ"
+// }
+function loadQuestion(question) {
+    console.log(question.id);
+    $("#sidebar-container-questions").append(
+        '<div id="sidebar-"'+ question.id+ ' class="Sidebar-Option">'+ question.title+ '</div>'
+    );
+}
