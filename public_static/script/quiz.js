26e61f2577495576c2d2dacd5c85c852f222ef4f
svr8 <shikhar.vaish90@gmail.com>
2018-08-05 02:34:18 +0530

    Building question loading script

--- public_static/script/quiz.js
+++ public_static/script/quiz.js
@@ -2,21 +2,27 @@ var questions = {
     "A": {    
         "id": "0",
         "title": "A",
-        "text": "Question0",
-        "code_template": "Template",
+        "description": "Question0",
         "constraints": {
             "var": {
-                "lower_bound": "1",
-                "name": "N",
-                "upper_bound": "10^9"
+                "0": {
+                    "lower_bound": "1",
+                    "name": "N",
+                    "upper_bound": "10^9"
+                }
             },
-            "others": [
+            "other": [
                 'All characters of the string are lowercase.'
             ]
-        }
+        },
+        "sample_input": "SAMPLE INPUT",
+        "sample_output": "SAMPLE OUTPUT",
+        "code_template": "Template",
+        "input": {}
     },
 };
 
+var selectedQuestionID = '';
 
 $(document).ready(function(){
     //Question Links
@@ -38,6 +44,60 @@ $(document).ready(function(){
 function loadQuestion(question) {
     console.log(question.id);
     $("#sidebar-container-questions").append(
-        '<div id="sidebar-"'+ question.id+ ' class="Sidebar-Option">'+ question.title+ '</div>'
+        getQuestionTabHTML(question)
     );
+
+    $('#sidebar-question-'+question.id).on('click', function() {
+        console.log('Question Tab: '+ question.id+ ' CLICKED');
+        // selectQuestionTab(question.id);
+    });
+}
+
+function displayQuestion(question) {
+
+    //If previously selected tab was a SOLUTION TAB
+    $('.content-solution').hide();
+
+    //Set Title
+    $('#question-title').html(question.title);
+
+    //Set Description
+    $('#question-description').html(question.description);
+
+    //Set Constraints
+    var constraint;
+
+    //-Clear Constraints
+    $('#question-constraints').html('');
+
+    //-Set variable constraints
+    constraint = question.constraints.var;
+    for(var i=0; i<constraint.length; i++) 
+        $('#question-constraints').append(getVariableConstraintHTML(constraint[i]));
+
+    //Set other constraints
+    constraint = question.constraints.other;
+    for(var i=0; i<constraint.length; i++) 
+        $('#question-constraints').append(getOtherConstraintHTML(constraint[i]));
+    
+    //Set Sample Input
+    //Set Sample Output
+}
+
+function getVariableConstraintHTML(constraint) {
+    return '<div class="constraints-item>'+ 
+                '<div class="constraints-lower_bound">'+constraint.lower_bound+'&lt;</div>'+
+                '<div class="constraints-name>'+constraint.name+'&lt;</div>'+
+                '<div class="constraints-upper_bound">'+constraints.upper_bound+'</div>'+
+           '</div>';
+}
+
+function getOtherConstraintHTML(constraint) {
+    return '<div class="constraints-item'+
+                '<div class="constraints-other">'+ constraint + '</div>'+
+           '</div>';
+}
+
+function getQuestionTabHTML(question) {
+    return '<div id="sidebar-question-"'+ question.id+ ' class="Sidebar-Option">'+ question.title+ '</div>';
 }
