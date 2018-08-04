964e49104583c5f919dcbe66436c327c16f9f69b
Shikhar <shikhar.vaish90@gmail.com>
2018-08-04 20:10:39 +0530

    Updated quiz page

--- script/quiz.js
+++ script/quiz.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     //Question Links
-    for(var i=0;i<n;i++) {
-        $("sidebar-questionIndex").on('click', function(){ console.log('Question Index Pressed'); });
-    }
+    // for(var i=0;i<n;i++) {
+    //     $("sidebar-questionIndex").on('click', function(){ console.log('Question Index Pressed'); });
+    // }
     
-    //Solution Links
-    for(var i=0;i<n;i++) {
-        $("sidebar-solutionIndex").on('click', function(){ console.log('Solution Index Pressed'); });
-    }
+    // //Solution Links
+    // for(var i=0;i<n;i++) {
+    //     $("sidebar-solutionIndex").on('click', function(){ console.log('Solution Index Pressed'); });
+    // }
 });
